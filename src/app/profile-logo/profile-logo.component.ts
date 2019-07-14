c8ff5d9d86f111f1d651c00d22f51fdc3ab480ea
Max Levitskiy <max.dstu@gmail.com>
2019-07-14 11:58:18 +0200

    username in profile logo menu

--- src/app/profile-logo/profile-logo.component.ts
+++ src/app/profile-logo/profile-logo.component.ts
@@ -10,27 +10,29 @@ import {environment} from '../../environments/environment';
 })
 export class ProfileLogoComponent implements OnInit {
 
-  private user: User;
-  private shortName: string;
-  private loginUrl: string = environment.authUrl + '/login?returnUnion=mydata';
-  private registerUrl: string = environment.authUrl + '/register?returnUnion=mydata';
+  private user: User = {};
+  private username: string;
 
   constructor(
     public authService: AuthService
   ) {
+  }
+  private shortName: string;
+  private loginUrl: string = environment.authUrl + '/login?returnUnion=mydata';
+  private registerUrl: string = environment.authUrl + '/register?returnUnion=mydata';
+
+  static generateShortName(user: User): string {
+    return user.username.slice(0, 2).toUpperCase();
   }
 
   ngOnInit() {
     if (this.authService.isLoggedIn()) {
       this.authService.getUser().then(user => {
         this.user = user;
-        this.shortName = this.generateShortName(user);
+        this.username = user.username;
+        this.shortName = ProfileLogoComponent.generateShortName(user);
       });
     }
-  }
-
-  private generateShortName(user: User): string {
-    return user.username.slice(0, 2).toUpperCase();
   }
 
 }
