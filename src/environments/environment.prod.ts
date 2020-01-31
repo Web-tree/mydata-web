b162f4ab0536152dda346eed7b741bd2f785f1bd
Max Levitskiy <max.dstu@gmail.com>
2020-01-31 12:07:40 +0100

    change api url for temp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authUrl: 'https://auth.webtree.org',
+  authUrl: 'https://auth-api.webtree.org',
   backendUrl: 'https://5cuh2ad5yh.execute-api.eu-central-1.amazonaws.com/prod'
 };
