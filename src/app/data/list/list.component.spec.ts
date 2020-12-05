2fd9f09cc81702e0993310b10c2e4a794362a75d
Max Levitskiy <max@webtree.org>
2020-12-05 09:23:10 +0100

    fix data list page if not logged in

--- src/app/data/list/list.component.spec.ts
+++ src/app/data/list/list.component.spec.ts
@@ -9,10 +9,12 @@ import {By} from '@angular/platform-browser';
 import {MatProgressSpinnerModule, MatTableModule} from '@angular/material';
 import {RouterTestingModule} from '@angular/router/testing';
 import {DataNamePipe} from '../../_pipes/data-name.pipe';
+import {AuthService} from '../../_services/auth.service';
 
 describe('ListComponent', () => {
   let fixture: ComponentFixture<ListComponent>;
   let dataService: SpyObj<DataService>;
+  let authService: SpyObj<AuthService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -30,51 +32,66 @@ describe('ListComponent', () => {
         {
           provide: DataService,
           useValue: jasmine.createSpyObj('DataService', ['getList'])
+        },
+        {
+          provide: AuthService,
+          useValue: jasmine.createSpyObj('AuthService', ['isLoggedIn'])
         }
       ]
     })
       .compileComponents();
     dataService = TestBed.get(DataService);
+    authService = TestBed.get(AuthService);
   }));
 
-  const mockDataList = (data) => dataService.getList.and.callFake(() => Promise.resolve<Data[]>(data));
 
-  describe('when data list is empty', () => {
-    beforeEach(async () => {
-      mockDataList([]);
-      fixture = TestBed.createComponent(ListComponent);
-      await fixture.whenStable();
-      fixture.detectChanges();
+  describe('when user logged in', async () => {
+    const mockDataList = (data) => dataService.getList.and.callFake(() => Promise.resolve<Data[]>(data));
+    fixture.detectChanges();
+    authService.isLoggedIn.and.returnValue(true);
+    describe('when data list is empty', () => {
+      beforeEach(async () => {
+        mockDataList([]);
+        fixture = TestBed.createComponent(ListComponent);
+        await fixture.whenStable();
+        fixture.detectChanges();
+      });
+      it('should create', () => {
+        expect(fixture.componentInstance).toBeTruthy();
+      });
+      it('should show message the empty list empty list', () => {
+        fixture.detectChanges();
+        expect(fixture.nativeElement.textContent).toContain(`You don't have data yet.`);
+      });
     });
-    it('should create', () => {
-      expect(fixture.componentInstance).toBeTruthy();
-    });
-    it('should show message the empty list empty list', () => {
-      fixture.detectChanges();
-      expect(fixture.nativeElement.textContent).toContain(`You don't have data yet.`);
+
+    describe('when list contains some data', () => {
+      beforeEach(async () => {
+        mockDataList([
+          {name: 'someName', value: 'someValue'}
+        ]);
+        fixture = TestBed.createComponent(ListComponent);
+        await fixture.whenStable();
+        fixture.detectChanges();
+      });
+      beforeEach(() => {
+        fixture.detectChanges();
+      });
+      it('should not show the empty list message', () => {
+        expect(fixture.nativeElement.textContent).not.toContain(`You don't have data yet.`);
+      });
+
+      xit('should show the data', () => {
+        const dataValue = fixture.debugElement.query(By.css('.mat-cell'));
+        expect(dataValue).toBeTruthy();
+      });
     });
   });
-
-  describe('when list contains some data', () => {
-    beforeEach(async () => {
-      mockDataList([
-        {name: 'someName', value: 'someValue'}
-      ]);
-      fixture = TestBed.createComponent(ListComponent);
-      await fixture.whenStable();
-      fixture.detectChanges();
-    });
-    beforeEach(() => {
-      fixture.detectChanges();
-    });
-    it('should not show the empty list message', () => {
-      expect(fixture.nativeElement.textContent).not.toContain(`You don't have data yet.`);
-    });
-
-    xit('should show the data', () => {
-      const dataValue = fixture.debugElement.query(By.css('.mat-cell'));
-      expect(dataValue).toBeTruthy();
+  describe('when user not logged in', async () => {
+    fixture.detectChanges();
+    authService.isLoggedIn.and.returnValue(false);
+    it('should show message', () => {
+      expect(fixture.nativeElement.textContent).toContain('Please login to see your data');
     });
   });
-
 });
