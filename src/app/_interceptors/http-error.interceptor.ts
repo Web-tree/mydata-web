6a71626edbf607fec3ac16556884d24a5eba20d3
Max Levitskiy <max@webtree.org>
2020-12-13 09:47:22 +0100

    Handle 401 error in HttpErrorInterceptor

--- src/app/_interceptors/http-error.interceptor.ts
+++ src/app/_interceptors/http-error.interceptor.ts
@@ -2,18 +2,27 @@ import {HTTP_INTERCEPTORS, HttpErrorResponse, HttpEvent, HttpHandler, HttpInterc
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {AlertService} from '../_services/alert.service';
+import {AuthService} from '../_services/auth.service';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
 
-  constructor(private alertService: AlertService) {
+  constructor(
+    private alertService: AlertService,
+    private authService: AuthService
+  ) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const observable = next.handle(req);
     observable.toPromise().catch(error => {
-      if (error instanceof HttpErrorResponse && (error.status === 0 || error.status.toString().startsWith('5'))) {
-        this.alertService.error(error.message);
+      if (error instanceof HttpErrorResponse) {
+        if (error.status === 0 || error.status.toString().startsWith('5')) {
+          this.alertService.error(error.message);
+        } else if (error.status === 401) {
+          this.alertService.error('Your session expired. Please login again.');
+          this.authService.logout();
+        }
       }
     });
     return observable;
